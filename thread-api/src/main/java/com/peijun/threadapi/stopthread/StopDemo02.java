6e57542fe1786227119ce9a7291784742676c315
Dylan Kwok <guosgbin@163.com>
2021-03-18 00:19:22 +0800

    修改stop测试

--- thread-api/src/main/java/com/peijun/threadapi/stopthread/StopDemo02.java
+++ thread-api/src/main/java/com/peijun/threadapi/stopthread/StopDemo02.java
@@ -8,21 +8,30 @@ import java.util.concurrent.TimeUnit;
  * <p>
  * 古之立大事者，不惟有超世之才，亦必有坚忍不拔之志——苏轼
  * <p>
- * 测试stop方法
+ * 测试stop方法 不释放锁
  */
 public class StopDemo02 {
-    public static void main(String[] args) {
+    private static final Object MONITOR = new Object();
+
+    public static void main(String[] args) throws InterruptedException {
         Thread t1 = new Thread(() -> {
-            System.out.println("子线程开始...");
-            try {
-                TimeUnit.SECONDS.sleep(3);
-            } catch (InterruptedException ignored) {
+            synchronized (MONITOR) {
+                System.out.println("子线程开始...");
+                try {
+                    TimeUnit.SECONDS.sleep(5);
+                } catch (InterruptedException ignored) {
+                }
             }
         });
-
-        long startTime = System.currentTimeMillis();
         t1.start();
-        t1.stop();
+        TimeUnit.SECONDS.sleep(1);
+        t1.stop(); // 停止线程t1
+        // 在主线程尝试获取锁
+        System.out.println("在主线程尝试获取锁...");
+        synchronized (MONITOR) {
+            System.out.println("主线程拿到锁...");
+        }
         System.out.println("子线程的状态：" + t1.getState());
+        System.out.println("结束...");
     }
 }

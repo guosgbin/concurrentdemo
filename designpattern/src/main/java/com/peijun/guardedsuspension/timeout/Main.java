57220cc5fd7d01c577a284c8f8ef99f5d472d364
Dylan Kwok <guosgbin@163.com>
2021-04-20 00:09:43 +0800

    带超时的保护性暂停

---
+++ designpattern/src/main/java/com/peijun/guardedsuspension/timeout/Main.java
@@ -0,0 +1,38 @@
+package com.peijun.guardedsuspension.timeout;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author: Dylan kwok GSGB
+ * @date: 2021/3/26 18:46
+ * <p>
+ * 古之立大事者，不惟有超世之才，亦必有坚忍不拔之志——苏轼
+ */
+public class Main {
+    public static void main(String[] args) {
+        MessageQueue messageQueue = new MessageQueue();
+        // 外卖骑手线程 送一份外卖
+        new Thread(() -> {
+            try {
+                System.out.println(Thread.currentThread().getName() + "开始运行...");
+                TimeUnit.SECONDS.sleep(1); // 模拟耗时
+                Message message = new Message("我是一个外卖");
+                System.out.println("骑手马上到你家门口 --> " + message.getName());
+                messageQueue.putMessage(message);
+            } catch (InterruptedException ignored) {
+            }
+        }, "外卖骑手").start();
+        // 点外卖的用户，等待外卖送达
+        new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + "开始运行...");
+            // 此时调用带超时的方法
+            Message message = messageQueue.getMessage(2000);
+            if (message == null) {
+                System.out.println("骑手也太磨叽了，我等两年了还没送到，我不等了，自己出去吃了");
+            } else {
+                System.out.println("哇，外卖到了，开吃开吃 --> " + message.getName());
+            }
+        }, "Bob").start();
+    }
+}
---
+++ designpattern/src/main/java/com/peijun/guardedsuspension/timeout/MessageQueue.java
@@ -0,0 +1,72 @@
+package com.peijun.guardedsuspension.timeout;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author: Dylan kwok GSGB
+ * @date: 2021/4/19 22:07
+ * <p>
+ * 古之立大事者，不惟有超世之才，亦必有坚忍不拔之志——苏轼
+ *
+ * 存放消息/请求的消息队列
+ */
+public class MessageQueue {
+    /**
+     * 存放请求的队列
+     */
+    private Queue<Message> queue = new LinkedList<>();
+
+    /**
+     * 从 请求/消息 队列中获取消息
+     * 假如队列中有消息，就取出返回，否则无限等待，等待其他线程唤醒
+     * @return 返回消息
+     */
+    public synchronized Message getMessage() {
+        // 守护方法
+        while (queue.peek() == null) {
+            // 队列中没有Message，需要去等待
+            try {
+                this.wait();
+            } catch (InterruptedException ignored) {
+            }
+        }
+        // 队列中有Message，拿出放回
+        return queue.poll();
+    }
+
+    /**
+     * 带超时时间的获取消息的请求，等待指定时间
+     * @return
+     */
+    public synchronized Message getMessage(long millis) {
+        long startTime = System.currentTimeMillis(); // 获取调用方法的时间
+        long wasteTime = 0; // 记录已消耗的时间
+        // 守护方法
+        while (queue.peek() == null) {
+            // 队列中没有Message，需要去等待
+            try {
+                long delay = millis - wasteTime; // 计算需要睡眠的时间
+                if (delay <= 0) {
+                    break;
+                }
+                // 睡眠指定时间
+                this.wait(delay);
+                // 计算已经耗时时间
+                wasteTime = System.currentTimeMillis() - startTime;
+            } catch (InterruptedException ignored) {
+            }
+        }
+        // 队列中有Message，拿出放回
+        return queue.poll();
+    }
+
+    /**
+     * 放入 请求/消息 至队列，唤醒等待的线程
+     */
+    public synchronized void putMessage(Message message) {
+        queue.offer(message);
+        this.notify();
+    }
+
+}
